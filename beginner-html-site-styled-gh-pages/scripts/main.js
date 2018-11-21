0bc58e3bdd94e61a5b8ce5d5c4363a23cfbb6842
zxlb <zhangawr@gmail.com>
2018-11-21 20:27:38 +0800

    delete ChangeUser

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -8,23 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute('src', 'images/firefox-icon.png');
     }
 }
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
